b7e480e13ed854983de59008a3bcf8e70fd5dc1e
Maeve Crossan <maevecrossan@gmail.com>
2024-09-11 14:05:10 +0000

    Add arrow function examples.

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,46 @@
-/**
- * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
- */
+// /**
+//  * To run this file in Gitpod, use the 
+//  * command node arrow-functions.js in the terminal
+//  */
 
-// Vanilla JavaScript Function
+// // // Vanilla JavaScript Function
+// // function addTwoNumbers(a, b) {
+// //     //Code block
+// //     return a + b;
+// // }
 
-// Arrow Function With Parameters
+// // let sum = addTwoNumbers(3,5);
+// // console.log(sum);
 
-// Single Line Arrow Function With Parameters
 
-// Implicit Returns
+// // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     //Code block
+//     return a + b;
+// }
 
-// Returning Multiple Lines
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
+
+// // Single Line Arrow Function With Parameters (Shortened version of above.)
+// //const addTwoNumbers2 = (a,b) => (a + b) **also valid
+const addTwoNumbers2 = (a,b) => a + b;
+let sum2 = addTwoNumbers2(6,4);
+ console.log(sum2)
+
+// // Implicit Returns
+// const saySomething = message => console.log(message);
+// saySomething("hello there!");
+
+// const sayHello = () => console.log('hello');
+// sayHello();
+
+// // Returning Multiple Lines
+// const returnMultipleLines = () => (
+//     '<p> 
+//         This is a multiline string!
+//     </p>'
+// )
+// console.log(returnMultipleLines());
+
+
